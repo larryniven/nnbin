be2f3b5c11cfd4af4cd09d93fc2a53b3a0c4aa96
Hao Tang <haotang@ttic.edu>
2017-01-02 22:26:03 -0600

    Move dropout to fully connected layers

--- frame-cnn-learn.cc
+++ frame-cnn-learn.cc
@@ -11,13 +11,18 @@
 #include "nn/cnn.h"
 #include <algorithm>
 
-std::shared_ptr<tensor_tree::vertex> make_tensor_tree(int layer)
+std::shared_ptr<tensor_tree::vertex> make_tensor_tree(int conv_layer, int fc_layer)
 {
     tensor_tree::vertex root { tensor_tree::tensor_t::nil };
 
-    root.children.push_back(cnn::make_cnn_tensor_tree(layer));
-    root.children.push_back(tensor_tree::make_tensor("softmax weight"));
-    root.children.push_back(tensor_tree::make_tensor("softmax bias"));
+    root.children.push_back(cnn::make_cnn_tensor_tree(conv_layer));
+
+    for (int i = 0; i < fc_layer; ++i) {
+        tensor_tree::vertex fc { tensor_tree::tensor_t::nil };
+        fc.children.push_back(tensor_tree::make_tensor("softmax weight"));
+        fc.children.push_back(tensor_tree::make_tensor("softmax bias"));
+        root.children.push_back(std::make_shared<tensor_tree::vertex>(fc));
+    }
 
     return std::make_shared<tensor_tree::vertex>(root);
 }
@@ -33,7 +38,8 @@ struct learning_env {
 
     std::shared_ptr<tensor_tree::vertex> var_tree;
 
-    int layer;
+    int conv_layer;
+    int fc_layer;
 
     double step_size;
     double decay;
@@ -113,8 +119,10 @@ learning_env::learning_env(std::unordered_map<std::string, std::string> args)
 
     std::ifstream param_ifs { args.at("param") };
     std::getline(param_ifs, line);
-    layer = std::stoi(line);
-    param = make_tensor_tree(layer);
+    std::vector<std::string> parts = ebt::split(line);
+    conv_layer = std::stoi(parts[0]);
+    fc_layer = std::stoi(parts[1]);
+    param = make_tensor_tree(conv_layer, fc_layer);
     tensor_tree::load_tensor(param, param_ifs);
     param_ifs.close();
 
@@ -202,16 +210,12 @@ void learning_env::run()
 
     int nsample = 0;
 
-    while (1) {
+    while (nsample < frame_batch.pos.size()) {
         std::vector<std::vector<double>> frames;
 
         frames = speech::load_frame_batch(frame_batch.at(nsample));
 
 	std::vector<std::string> labels = speech::load_label_batch(label_batch.at(nsample));
-
-        if (nsample >= frame_batch.pos.size()) {
-            break;
-        }
 
         std::cout << "sample: " << nsample << std::endl;
 
@@ -233,23 +237,12 @@ void learning_env::run()
 
         cnn::multilayer_transcriber trans;
 
-        for (int i = 0; i < layer; ++i) {
-            if (ebt::in(std::string("dropout"), args)) {
-                trans.layers.push_back(std::make_shared<cnn::dropout_transcriber>(
-                    cnn::dropout_transcriber {
-                        std::make_shared<cnn::cnn_transcriber>(cnn::cnn_transcriber{}),
-                        dropout, gen
-                    }
-                ));
-            } else {
-                trans.layers.push_back(std::make_shared<cnn::cnn_transcriber>(
-                    cnn::cnn_transcriber{}));
-            }
+        for (int i = 0; i < conv_layer; ++i) {
+            trans.layers.push_back(std::make_shared<cnn::cnn_transcriber>(
+                cnn::cnn_transcriber{}));
         }
 
         std::shared_ptr<autodiff::op_t> feat = trans(input, var_tree->children[0]);
-
-        feat = autodiff::emul(feat, autodiff::dropout_mask(feat, dropout, gen));
 
         auto& t = tensor_tree::get_tensor(param->children[0]->children.back()->children.back());
 
@@ -257,11 +250,28 @@ void learning_env::run()
             { (unsigned int) frames.size(), (unsigned int) frames.front().size() * t.size(0) });
 
         std::vector<std::shared_ptr<autodiff::op_t>> logprob;
+
         for (int t = 0; t < frames.size(); ++t) {
-            logprob.push_back(autodiff::logsoftmax(autodiff::add(
-                tensor_tree::get_var(var_tree->children[2]),
-                autodiff::mul(autodiff::row_at(feat, t), tensor_tree::get_var(var_tree->children[1]))
-            )));
+            std::shared_ptr<autodiff::op_t> feat_t = autodiff::row_at(feat, t);
+
+            for (int i = 0; i < fc_layer - 1; ++i) {
+                if (ebt::in(std::string("dropout"), args)) {
+                    feat_t = autodiff::emul(feat_t, autodiff::dropout_mask(feat_t, dropout, gen));
+                }
+
+                feat_t = autodiff::relu(autodiff::add(
+                    autodiff::mul(feat_t, tensor_tree::get_var(var_tree->children[i + 1]->children[0])),
+                    tensor_tree::get_var(var_tree->children[i + 1]->children[1])
+                ));
+            }
+
+            logprob.push_back(autodiff::logsoftmax(
+                autodiff::add(
+                    autodiff::mul(feat_t,
+                        tensor_tree::get_var(var_tree->children[fc_layer]->children[0])),
+                    tensor_tree::get_var(var_tree->children[fc_layer]->children[1])
+                )
+            ));
         }
 
         double loss_sum = 0;
@@ -338,7 +348,7 @@ void learning_env::run()
             logprob[0]->grad = std::make_shared<la::tensor<double>>(loss.grad());
             autodiff::grad(logprob[0], autodiff::grad_funcs);
 
-            std::shared_ptr<tensor_tree::vertex> grad = make_tensor_tree(layer);
+            std::shared_ptr<tensor_tree::vertex> grad = make_tensor_tree(conv_layer, fc_layer);
             tensor_tree::copy_grad(grad, var_tree);
             auto grad_pre_order = leaves_pre_order(grad);
 
@@ -376,12 +386,12 @@ void learning_env::run()
 
         std::cout << "loss: " << loss_sum / nframes << std::endl;
 
-        std::shared_ptr<tensor_tree::vertex> grad = make_tensor_tree(layer);
+        std::shared_ptr<tensor_tree::vertex> grad = make_tensor_tree(conv_layer, fc_layer);
         tensor_tree::copy_grad(grad, var_tree);
 
         double n = tensor_tree::norm(grad);
 
-        std::cout << "norm: " << n << std::endl;
+        std::cout << "grad norm: " << n << std::endl;
 
         if (ebt::in(std::string("clip"), args)) {
             if (n > clip) {
@@ -413,12 +423,12 @@ void learning_env::run()
     }
 
     std::ofstream param_ofs { output_param };
-    param_ofs << layer << std::endl;
+    param_ofs << conv_layer << " " << fc_layer << std::endl;
     tensor_tree::save_tensor(param, param_ofs);
     param_ofs.close();
 
     std::ofstream opt_data_ofs { output_opt_data };
-    opt_data_ofs << layer << std::endl;
+    opt_data_ofs << conv_layer << " " << fc_layer << std::endl;
     opt->save_opt_data(opt_data_ofs);
     opt_data_ofs.close();
 }
--- frame-cnn-predict.cc
+++ frame-cnn-predict.cc
@@ -10,13 +10,18 @@
 #include "nn/tensor-tree.h"
 #include "nn/cnn.h"
 
-std::shared_ptr<tensor_tree::vertex> make_tensor_tree(int layer)
+std::shared_ptr<tensor_tree::vertex> make_tensor_tree(int conv_layer, int fc_layer)
 {
     tensor_tree::vertex root { tensor_tree::tensor_t::nil };
 
-    root.children.push_back(cnn::make_cnn_tensor_tree(layer));
-    root.children.push_back(tensor_tree::make_tensor("softmax weight"));
-    root.children.push_back(tensor_tree::make_tensor("softmax bias"));
+    root.children.push_back(cnn::make_cnn_tensor_tree(conv_layer));
+
+    for (int i = 0; i < fc_layer; ++i) {
+        tensor_tree::vertex fc { tensor_tree::tensor_t::nil };
+        fc.children.push_back(tensor_tree::make_tensor("softmax weight"));
+        fc.children.push_back(tensor_tree::make_tensor("softmax bias"));
+        root.children.push_back(std::make_shared<tensor_tree::vertex>(fc));
+    }
 
     return std::make_shared<tensor_tree::vertex>(root);
 }
@@ -29,7 +34,8 @@ struct prediction_env {
 
     std::shared_ptr<tensor_tree::vertex> var_tree;
 
-    int layer;
+    int conv_layer;
+    int fc_layer;
 
     std::vector<std::string> id_label;
     std::unordered_map<std::string, int> label_id;
@@ -82,8 +88,10 @@ prediction_env::prediction_env(std::unordered_map<std::string, std::string> args
 
     std::ifstream param_ifs { args.at("param") };
     std::getline(param_ifs, line);
-    layer = std::stoi(line);
-    param = make_tensor_tree(layer);
+    std::vector<std::string> parts = ebt::split(line);
+    conv_layer = std::stoi(parts[0]);
+    fc_layer = std::stoi(parts[1]);
+    param = make_tensor_tree(conv_layer, fc_layer);
     tensor_tree::load_tensor(param, param_ifs);
     param_ifs.close();
 
@@ -126,7 +134,7 @@ void prediction_env::run()
 
         cnn::multilayer_transcriber trans;
 
-        for (int i = 0; i < layer; ++i) {
+        for (int i = 0; i < conv_layer; ++i) {
             trans.layers.push_back(std::make_shared<cnn::cnn_transcriber>(
                 cnn::cnn_transcriber{}));
         }
@@ -139,11 +147,24 @@ void prediction_env::run()
             { (unsigned int) frames.size(), (unsigned int) frames.front().size() * t.size(0) });
 
         std::vector<std::shared_ptr<autodiff::op_t>> logprob;
+
         for (int t = 0; t < frames.size(); ++t) {
-            logprob.push_back(autodiff::logsoftmax(autodiff::add(
-                tensor_tree::get_var(var_tree->children[2]),
-                autodiff::mul(autodiff::row_at(feat, t), tensor_tree::get_var(var_tree->children[1]))
-            )));
+            std::shared_ptr<autodiff::op_t> feat_t = autodiff::row_at(feat, t);
+
+            for (int i = 0; i < fc_layer - 1; ++i) {
+                feat_t = autodiff::relu(autodiff::add(
+                    autodiff::mul(feat_t, tensor_tree::get_var(var_tree->children[i + 1]->children[0])),
+                    tensor_tree::get_var(var_tree->children[i + 1]->children[1])
+                ));
+            }
+
+            logprob.push_back(autodiff::logsoftmax(
+                autodiff::add(
+                    autodiff::mul(feat_t,
+                        tensor_tree::get_var(var_tree->children[fc_layer - 1]->children[0])),
+                    tensor_tree::get_var(var_tree->children[fc_layer - 1]->children[1])
+                )
+            ));
         }
 
         double loss_sum = 0;
